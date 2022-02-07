fd3cc5a18188027daf00fa0f0d1f77773fb38d48
karoltomek <karoltomek@o2.pl>
2022-02-07 17:54:24 +0100

    CompletableFuture six lesson v2

--- src/main/java/com/kt/lekcje/tydzien5/CompletableFutureLesson6.java
+++ src/main/java/com/kt/lekcje/tydzien5/CompletableFutureLesson6.java
@@ -1,5 +1,8 @@
 package com.kt.lekcje.tydzien5;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
@@ -9,53 +12,51 @@ class CompletableFutureLesson6 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         System.out.println(Thread.currentThread().getName() + " Starting...");
 
-        CompletableFuture<Long> asyncLong = CompletableFuture.supplyAsync(() -> {
-            try {
-                System.out.println(Thread.currentThread().getName() + " Computing...");
-                Thread.sleep(1500);
-                System.out.println(Thread.currentThread().getName() + " DONE");
-                return 42L;
+        CompletableFuture<String> klockiAsync = new AsyncTask("klocki").fetch();
+        CompletableFuture<String> tasmaAsync = new AsyncTask("tasma").fetch();
+        CompletableFuture<String> papierAsync = new AsyncTask("papier").fetch();
 
-            } catch (InterruptedException e) {
-                throw new IllegalStateException();
+        final CompletableFuture<List<String>> future = klockiAsync.thenCombine(tasmaAsync, (klocki, tasma) -> {
+            List<String> list = new ArrayList<>();
+            list.add(klocki);
+            list.add(tasma);
+            return list;
+        }).thenCombine(papierAsync, (list, papier) -> {
+            list.add(papier);
+            return list;
+        }).whenComplete((list, error) -> {
+            if (error != null) {
+                System.err.println(Thread.currentThread().getName() + " Something went wrong. Unable to proceed with your order.");
+            } else {
+                System.out.println(Thread.currentThread().getName() + " Parcel: " + String.join(", ", list));
             }
         });
 
-        CompletableFuture<Long> asyncLong2 = CompletableFuture.supplyAsync(() -> {
-            try {
-                System.out.println(Thread.currentThread().getName() + " Computing...");
-                Thread.sleep(1500);
-                System.out.println(Thread.currentThread().getName() + " DONE");
-                return 42L;
-
-            } catch (InterruptedException e) {
-                throw new IllegalStateException();
-            }
-        });
-
-        Executor executor = Executors.newSingleThreadExecutor();
-
-        final CompletableFuture<Void> future = asyncLong.thenAcceptAsync(value -> {
-            try {
-                System.out.println(Thread.currentThread().getName() + " Accepting value...");
-                Thread.sleep(200);
-                System.out.println(Thread.currentThread().getName() + " Consumed value is: " + value);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }, executor);
-
-        asyncLong.get();
-        //future.get();
-
-        CompletableFuture<Long> completableFuture = new CompletableFuture<>();
-        //completableFuture.complete(99L);
-        completableFuture.completeExceptionally(new RuntimeException("Ups..."));
-        completableFuture.get();
+        future.get();
 
         System.out.println(Thread.currentThread().getName() + " DONE");
 
 
     }
 
+    static class AsyncTask {
+        private String result;
+
+        AsyncTask(String result) {
+            this.result = result;
+        }
+
+        public CompletableFuture<String> fetch() {
+            return CompletableFuture.supplyAsync(() -> {
+                try {
+                    System.out.println(Thread.currentThread().getName() + " Preparing result: " + result + "...");
+                    Thread.sleep(500);
+                    System.out.println(Thread.currentThread().getName() + " DONE " + result);
+                    return result;
+                } catch (InterruptedException e) {
+                    throw new IllegalStateException(e);
+                }
+            });
+        }
+    }
 }
